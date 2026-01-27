c35710a56150e37eb616f058a4b253bd88db6271
miu <38824651+miu200521358@users.noreply.github.com>
2026-01-27 23:12:07 +0900

    usecase 呼び出し

--- pkg/usecase/load.go
+++ pkg/usecase/load.go
@@ -4,16 +4,15 @@ import (
 	"github.com/miu200521358/mlib_go/pkg/adapter/io_common"
 	"github.com/miu200521358/mlib_go/pkg/domain/model"
 	"github.com/miu200521358/mlib_go/pkg/domain/motion"
-
-	"github.com/miu200521358/mu_motion_viewer/pkg/workflow"
+	commonusecase "github.com/miu200521358/mlib_go/pkg/usecase"
 )
 
 // LoadModel はモデルを読み込み、型を検証して返す。
 func LoadModel(rep io_common.IFileReader, path string) (*model.PmxModel, error) {
-	return workflow.LoadModel(rep, path)
+	return commonusecase.LoadModel(rep, path)
 }
 
 // LoadMotion はモーションを読み込み、型を検証して返す。
 func LoadMotion(rep io_common.IFileReader, path string) (*motion.VmdMotion, error) {
-	return workflow.LoadMotion(rep, path)
+	return commonusecase.LoadMotion(rep, path)
 }
